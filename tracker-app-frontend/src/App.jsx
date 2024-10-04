38cbf3079f7cfb85c8b56cd7c864f79859e62673
LathithaKunana <lathitha.kunana@younglings.africa>
2024-10-04 18:27:06 +0200

    added movement tracking component

--- tracker-app-frontend/src/App.jsx
+++ tracker-app-frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import StepTracker from './components/StepTracker';
 import NoiseLevelTracker from './components/NoiseLevelTracker';
+import MovementTracker from './components/MovementTracker';
 
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col gap-4 items-center mt-8">
       <StepTracker />
       <NoiseLevelTracker />
+      <MovementTracker />
     </div>
   );
 };
